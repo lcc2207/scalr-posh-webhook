b5804844ad2ac0469586b679399daefdbd00a052
lcc2207 <lynn@fidmail.com>
2019-01-18 09:47:12 -0600

    add logging to hostname

--- test_scripts/hostname.ps1
+++ test_scripts/hostname.ps1
@@ -1,4 +1,12 @@
+$logfile = "C:\scripts\logme.txt"
+
+function logme( $message ) {
+    Write-Output "$(Get-TimeStamp): $message" | Out-file $logfile -append
+}
+
 $type = Get-Random -InputObject "DEV", "PROD", "QA"
 $num  = Get-Random 5
 $hostname = ConvertTo-Json @{hostname="$type$num"}
 Write-Output $hostname
+
+logme "Assigning ServerName $hostname"
